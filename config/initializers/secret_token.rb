62567a31092beec5f60de19776f02a267091e0a1
2husher <what_or_the@bk.ru>
2013-02-14 23:20:51 +0300

    Initial commit
    Signed-off-by: 2husher <what_or_the@bk.ru>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a20f14319839f871368ed7992800e661232b4ff969b8615c856a91612df4412bf5296cb5482f7b8d651f853b72f74b00cd0183e8e6bb8ee2ac9625597f4671c0'
