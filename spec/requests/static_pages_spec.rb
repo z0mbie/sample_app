8106178a1e75d8986f9f41f4f09a2530716e6143
2husher <what_or_the@bk.ru>
2013-02-15 15:17:15 +0300

    Add Contact page
    Signed-off-by: 2husher <what_or_the@bk.ru>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,16 @@ describe "Static Pages" do
         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
